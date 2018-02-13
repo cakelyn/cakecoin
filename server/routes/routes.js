560fca9d625d421b9b4d3008a69a7b7c2a7d1034
Katelyn Stamas <cakelyn@gmail.com>
2018-02-13 17:45:43 -0500

    :sparkles: Add transaction and mine routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,9 @@
+/*
+TODO:
+route for getting Cakecoin balance of a user
+route for testing if chain is valid?
+ */
+
 const router = require('express').Router();
 const Cakecoin = require('./../middleware/cakecoin.js');
 
@@ -6,5 +12,12 @@ const responseMiddleware = (req, res, next) => {
 };
 
 router.get('/chain', Cakecoin.getChain, responseMiddleware);
+// curl http://127.0.0.1:1337/chain
+
+router.post('/transaction', Cakecoin.newTransaction, responseMiddleware);
+// curl -d '{ 'address1', 'address2', '100' }' -X POST http://127.0.0.1:1337/transaction
+
+router.post('/mine', Cakecoin.mine, responseMiddleware);
+// curl -d '{ 'address' }' -X POST http://127.0.0.1:1337/mine
 
 module.exports = router;
