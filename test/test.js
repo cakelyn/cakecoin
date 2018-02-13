3ea0b8b05f6a51c634804b56787fafff8fcb922c
Katelyn Stamas <cakelyn@gmail.com>
2018-02-12 23:22:34 -0500

    :white_check_mark: Set up test folder

---
+++ test/test.js
@@ -0,0 +1,6 @@
+/*
+WHAT TO TEST:
+- /getChain should return chain with only genesis block
+- /mine should mine a new block and add into the chain
+- /transaction/new should add the new transaction to the next mining
+ */
